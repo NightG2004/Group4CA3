d67494ae1843aa876708b26419acb2ae5403e48a
Night G <test@email>
2025-03-13 00:45:17 +0800

    initial commit

---
+++ js/form.js
@@ -0,0 +1,17 @@
+function validateForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let industry = document.getElementById("industry").value;
+
+    if (name === "" || email === "" || industry === "") {
+        alert("All fields must be filled out!");
+        return false;
+    }
+
+    let successMessage = `Thank you for your interest in the ${industry} industry!
+Name: ${name}
+Email Address: ${email}"`;
+    alert(successMessage);
+    window.location.href = "page7.html"; // Redirect to successful submission page
+    return false; // Prevent form submission
+}
